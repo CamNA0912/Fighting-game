ebe3f964cbfc4e5dec5309a0e62a8146d957b3a7
Cam <nghiem9155@gmail.com>
2022-06-14 22:40:50 +0700

    Player & enemy health bar interaction

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ class Sprite {
             }
         this.color = color
         this.isAttacking
+        this.health = 100
     }
 
     draw() {
@@ -160,7 +161,8 @@ function animate() {
     })
         && player.isAttacking) {
         player.isAttacking = false
-        document.querySelector('#enemyHealth').style.width = '200'
+        enemy.health -= 20
+        document.querySelector('#enemyHealth').style.width = enemy.health + '%'
     }
 
     if (rectangularCollision({
@@ -169,7 +171,8 @@ function animate() {
     })
         && enemy.isAttacking) {
         enemy.isAttacking = false
-        console.log('enemy attack successful')
+        player.health -= 20
+        document.querySelector('#playerHealth').style.width = player.health + '%'
     }
 }
 
