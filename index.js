de8593bf0f8ef9039d5f05a22402f09ba7866326
Cam <nghiem9155@gmail.com>
2022-06-16 17:52:06 +0700

    Background sprite

--- index.js
+++ index.js
@@ -9,6 +9,14 @@ c.fillRect(0, 0, canvas.width, canvas.height)
 
 const gravity = 0.7
 
+const background = new Sprite({
+    position: {
+        x: 0,
+        y: 0
+    },
+    imageSrc: './img/background.png'
+})
+
 //Image of player
 const player = new Fighter({
     position: {
@@ -62,43 +70,6 @@ const keys = {
     }
 }
 
-function rectangularCollision({ rectangle1, rectangle2 }) {
-    return (
-        rectangle1.attackBox.position.x + rectangle1.attackBox.width >= rectangle2.position.x 
-        &&  rectangle1.attackBox.position.x <= rectangle2.position.x + rectangle2.width 
-        && rectangle1.attackBox.position.y + rectangle1.attackBox.height >= rectangle2.position.y 
-        && rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height
-    )
-}
-
-// Game over screen
-function determineWinner({player, enemy, timerId }) {
-   clearTimeout(timerId)
-    document.querySelector('#displayText').style.display = 'flex'
-    if (player.health === enemy.health) {
-        document.querySelector('#displayText').innerHTML = 'Tie'
-        document.querySelector('#displayText').style.display = 'flex'
-        } else if (player.health > enemy.health) {
-            document.querySelector('#displayText').innerHTML = 'Player 1 win'        
-        } else if (player.health < enemy.health) {
-            document.querySelector('#displayText').innerHTML = 'Player 2 win' 
-        }
-}
-
-// Set timer
-let timer = 60
-let timerId
-function decreaseTimer() {
-    if (timer > 0) {
-        timerId = setTimeout(decreaseTimer, 1000)
-        timer--
-        document.querySelector('#timer').innerHTML = timer
-    }
-
-    if (timer === 0) {
-        determineWinner({player, enemy, timerId})
-    }
-}
 
 decreaseTimer()
 
@@ -106,6 +77,7 @@ function animate() {
     window.requestAnimationFrame(animate)
     c.fillStyle = 'black'
     c.fillRect(0, 0, canvas.width, canvas.height)
+    background.update()
     player.update()
     enemy.update()
 
