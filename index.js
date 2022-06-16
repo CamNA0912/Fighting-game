cbe72dd3f6685cd8aa432709236bc2619d8659e5
Cam <nghiem9155@gmail.com>
2022-06-16 17:41:03 +0700

    Add another file

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const canvas = document.querySelector('canvas');
 const c = canvas.getContext('2d')
 
@@ -8,68 +9,8 @@ c.fillRect(0, 0, canvas.width, canvas.height)
 
 const gravity = 0.7
 
-class Sprite {
-    constructor({ position, velocity, color = 'red' , offset}) {
-        this.position = position
-        this.velocity = velocity
-        this.width = 50
-        this.height = 150
-        this.lastKey
-        this.attackBox = {
-            position: {
-                x: this.position.x,
-                y: this.position.y
-            },
-            offset,
-            width: 100,
-            height: 50
-            }
-        this.color = color
-        this.isAttacking
-        this.health = 100
-    }
-
-    draw() {
-        c.fillStyle= this.color
-        c.fillRect(
-            this.position.x, 
-            this.position.y, this.width, 
-            this.height)
-
-        // attack box
-        if (this.isAttacking) {
-        c.fillStyle= 'gold'
-        c.fillRect( 
-            this.attackBox.position.x, 
-            this.attackBox.position.y, 
-            this.attackBox.width, 
-            this.attackBox.height)
-        }
-    }
-    
-    update() {
-        this.draw()
-        this.attackBox.position.x = this.position.x + this.attackBox.offset.x
-        this.attackBox.position.y = this.position.y
-        
-        this.position.x += this.velocity.x
-        this.position.y += this.velocity.y
-
-        if (this.position.y + this.height + this.velocity.y >= canvas.height) {
-            this.velocity.y = 0
-        } else this.velocity.y += gravity
-    }
-
-    attack() {
-        this.isAttacking = true
-        setTimeout(() => {
-            this.isAttacking = false
-        }, 100)
-    }
-}
-
 //Image of player
-const player = new Sprite({
+const player = new Fighter({
     position: {
         x: 0,
         y: 0
@@ -85,7 +26,7 @@ const player = new Sprite({
 })
 
 //Image of enemy
-const enemy = new Sprite({
+const enemy = new Fighter({
     position: {
         x: 400,
         y: 100
@@ -130,6 +71,7 @@ function rectangularCollision({ rectangle1, rectangle2 }) {
     )
 }
 
+// Game over screen
 function determineWinner({player, enemy, timerId }) {
    clearTimeout(timerId)
     document.querySelector('#displayText').style.display = 'flex'
@@ -143,6 +85,7 @@ function determineWinner({player, enemy, timerId }) {
         }
 }
 
+// Set timer
 let timer = 60
 let timerId
 function decreaseTimer() {
